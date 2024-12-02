181464a1cd8dd05f7410dc35fb4fb41e4cb1f25f
ionutbleoju11 <119681452+ionutbleoju11@users.noreply.github.com>
2024-12-02 20:44:14 +0100

    Add files via upload

---
+++ EstructuraDeDatos/EstructuraDeDatos/adadasgdahshdh.java
@@ -0,0 +1,11 @@
+package EstructuraDeDatos;
+
+public class adadasgdahshdh {
+    public static void main(String[]args){
+        System.out.print("Introduce un numero: ");
+
+        int numero = 10;
+
+        System.out.println(numero);
+    }
+}
---
+++ EstructuraDeDatos/EstructuraDeDatos/asdadadad.java
@@ -0,0 +1,7 @@
+package EstructuraDeDatos;
+
+public class asdadadad {
+    public static void main(String[]args){
+
+    }
+}
